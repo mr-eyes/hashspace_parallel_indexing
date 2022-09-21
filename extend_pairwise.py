489c8c00c544057c8397750a962ee3976ce60ef9
mr-eyes <mabuelanin@gmail.com>
2022-09-21 11:43:30 -0700

    fix

--- extend_pairwise.py
+++ extend_pairwise.py
@@ -42,7 +42,7 @@ with open(pairwise_tsv) as ORIGINAL, open(new_pairwise_tsv, 'w') as NEW:
         min_containment = shared_kmers / max(id_to_kmer_count[id_1],id_to_kmer_count[id_2])
         avg_containment = (max_containment + min_containment) / 2
         n_unique_kmers = (id_to_kmer_count[id_1] + id_to_kmer_count[id_2]) / 2
-        ani = to_ani(avg_containment, kSize, metadata_dict["scale"], n_unique_kmers=n_unique_kmers)
+        ani = to_ani(avg_containment, kSize, metadata_dict["scale"], n_unique_kmers=n_unique_kmers).ani
         NEW.write(f"{or_line.strip()}\t{avg_containment}\t{ani}\n")
         
         
